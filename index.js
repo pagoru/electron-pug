6df11f10ead7f4dde476176ed907ba5ca61d07f6
Pablo González <pagoru@gmail.com>
2017-07-26 19:22:04 +0200

    less integration

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const path = require('path');
 const pug = require('pug');
 const {_extend: extend} = require('util');
 const mime = require('mime');
+const less = require('less');
 
 
 /**
@@ -21,17 +22,17 @@ const mime = require('mime');
  * @return {String} path to file
  */
 const getPath = url => {
-  let parsed = require('url').parse(url);
-  let result = decodeURIComponent(parsed.pathname);
+    let parsed = require('url').parse(url);
+    let result = decodeURIComponent(parsed.pathname);
 
-  // Local files in windows start with slash if no host is given
-  // file:///c:/something.pug
-  if(process.platform === 'win32' && !parsed.host.trim()) {
-    result = result.substr(1);
-  }
+    // Local files in windows start with slash if no host is given
+    // file:///c:/something.pug
+    if(process.platform === 'win32' && !parsed.host.trim()) {
+        result = result.substr(1);
+    }
 
-  return result;
-}
+    return result;
+};
 
 /**
  * Callback handler for 'interceptBufferProtocol'.
@@ -41,49 +42,43 @@ const getPath = url => {
  * @param {Error} error not undefined if any error happens
  */
 const interceptCB = error => {
-  if (!error) {
-    console.log('Pug interceptor registered successfully');
-  } else {
-    console.error('Pug interceptor failed:', error);
-  }
-}
+    if (!error) {
+        console.log('Pug & less interceptor registered successfully');
+    } else {
+        console.error('Pug & less interceptor failed:', error);
+    }
+};
 
-module.exports = function(pugOptions, locals) {
-  app.on('ready', () => {
-    let options = extend({}, pugOptions || {});
+module.exports = (pugOptions, locals) => {
+    app.on('ready', () => {
+        let options = extend({}, pugOptions || {});
 
-    protocol.interceptBufferProtocol('file', (request, callback) => {
-      let file = getPath(request.url);
+        protocol.interceptBufferProtocol('file', (request, callback) => {
+            let file = getPath(request.url);
 
-      // See if file actually exists
-      try {
-        let content = fs.readFileSync(file);
+            // See if file actually exists
+            try {
+                let content = fs.readFileSync(file);
 
-        let ext = path.extname(file);
-        if (ext === '.pug') {
-          let compiled = pug.compileFile(file, pugOptions)(locals);
+                let ext = path.extname(file);
 
-          return callback({data: new Buffer(compiled), mimeType:'text/html'});
-        } else {
-          return callback({data: content, mimeType: mime.lookup(ext)});
-        }
-      } catch (e) {
-        // All error wrt. Pug are rendered in browser
-        if (e.code.startsWith('PUG:')) {
-          return callback({data: new Buffer(`<pre style="tab-size:1">${e}</pre>`), mimeType:'text/html'});
-        }
+                switch (ext){
+                    case '.pug':
+                        let compiled = pug.compileFile(file, pugOptions)(locals);
 
-        // See here for error numbers:
-        // https://code.google.com/p/chromium/codesearch#chromium/src/net/base/net_error_list.h
-        if (e.code === 'ENOENT') {
-         // NET_ERROR(FILE_NOT_FOUND, -6)
-         return callback(6);
-        }
-
-        // All other possible errors return a generic failure
-        // NET_ERROR(FAILED, -2)
-        return callback(2);
-      }
-    }, interceptCB);
-  });
+                        return callback({data: new Buffer(compiled), mimeType:'text/html'});
+                    case '.less':
+                        less.render(content.toString(), (error, compiled) => {
+                            if(error) callback(error);
+                            return callback({data: new Buffer(compiled.css), mimeType:'text/css'});
+                        });
+                        break;
+                    default:
+                        return callback({data: content, mimeType: mime.lookup(ext)});
+                }
+            } catch (e) {
+                return callback(e);
+            }
+        }, interceptCB);
+    });
 };
